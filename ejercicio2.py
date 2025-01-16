00b26d9ecb76c2b680d87a34a6b4f3d196154dc6
LCA675 <lauracalleanguita@gmail.com>
2025-01-16 23:41:15 +0100

    Ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+numero1=float(input("Introduzca un número: "))
+numero2=float(input("Introduzca otro número: "))
+resultado= numero1 + numero2
+print("La suma de los números introducidos son: ", resultado)
